93e4ebc9e8afb07390c9cc15d1b186b89bc18c37
kapit0shka <neverlle98@gmail.com>
2022-04-11 21:10:42 +0500

    add links to all routs

--- src/pages/chat/chat.tmpl.ts
+++ src/pages/chat/chat.tmpl.ts
@@ -41,6 +41,29 @@ export default
           </a>
         {{/each}}
       </ul>
+      <p>
+        <a href='/'>Вход</a>
+        <br>
+        <a href='/registration'>Регистрация</a>
+        <br>
+        <a href='/notSelectedChat'>Пустой чат</a>
+        <br>
+        <a href='/chatSelected'>Чат с сообщениями</a>
+        <br>
+        <a href='/viewProfile'>Профиль</a>
+        <br>
+        <a href='/editProfileData'>Редактирование профиля</a>
+        <br>
+        <a href='/editProfilePassword'>Изменение пароля</a>
+        <br>
+        <a href='/forbidden'>403</a>
+        <br>
+        <a href='/notFound'>404</a>
+        <br>
+        <a href='/internalServerError'>500</a>
+        <br>
+        <a href='/serviceUnavailable'>503</a>
+      </p>
     </div>
     {{{currentChatArea}}}
   </div>
